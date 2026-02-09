d69c90853a2f60071506f6cc0dccbf0993d5036a
Maxime Albertelli <91539486+Maxime-Albertelli@users.noreply.github.com>
2026-02-09 22:15:25 +0100

    Ajout de commentaire informant sur le fonctionnement initial du bot ainsi que quelques fonction de base

--- MyBot.cpp
+++ MyBot.cpp
@@ -23,35 +23,36 @@ int main(int argc, char* argv[]) {
     }
     mt19937 rng(rng_seed);
 
-    Game game;
+    Game game; //initialisation d'une partie (une partie comporte un joueur (nous) avec son ID, une liste d'autre joueurs, une map, le tour actuel)
+    
     // At this point "game" variable is populated with initial map data.
     // This is a good place to do computationally expensive start-up pre-processing.
     // As soon as you call "ready" function below, the 2 second per turn timer will start.
-    game.ready("MyCppBot");
-
-    LOG("Toto")
+    game.ready("Mon pote"); 
 
     for (;;) {
         game.update_frame();
-        shared_ptr<Player> me = game.me;
+        shared_ptr<Player> me = game.me; 
         unique_ptr<GameMap>& game_map = game.game_map;
 
         vector<Command> command_queue;
 
-        for (const auto& ship_iterator : me->ships) {
+        /*Gestion des vaisseaux*/
+        for (const auto& ship_iterator : me->ships) { //Pour chaque vaisseau de notre bot
             shared_ptr<Ship> ship = ship_iterator.second;
-            if (game_map->at(ship)->halite < constants::MAX_HALITE / 10 || ship->is_full()) {
-                Direction random_direction = ALL_CARDINALS[rng() % 4];
-                command_queue.push_back(ship->move(random_direction));
+            if (game_map->at(ship)->halite < constants::MAX_HALITE / 10 || ship->is_full()) { //Si le vaisseau est plein ou qu'il a fini de remplir 1/10 de sa capacité
+                Direction random_direction = ALL_CARDINALS[rng() % 4]; //Choix d'une direction aléatoire
+                command_queue.push_back(ship->move(random_direction)); //Le vaisseau se dirige dans une direction aléatoire
             } else {
-                command_queue.push_back(ship->stay_still());
+                command_queue.push_back(ship->stay_still()); //Reste sur place
             }
         }
 
+        /*Gestion du vaisseau mère*/
         if (
             game.turn_number <= 200 &&
             me->halite >= constants::SHIP_COST &&
-            !game_map->at(me->shipyard)->is_occupied())
+            !game_map->at(me->shipyard)->is_occupied()) //Si on est à moins de 200 tours, qu'on a assez de ressource pour faire apparaitre et que la case du vaisseau est libre => faire apparaitre
         {
             command_queue.push_back(me->shipyard->spawn());
         }
@@ -63,3 +64,31 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+/*
+Détails de la stratégie actuelle :
+    Le vaisseau mère fait apparaitre des petits vaisseau dès qu'il a assez de ressource (si case libre et avant 200 tours).
+    Les petits vaisseaux se déplacent ensuite aléatoirement, récupèrent un dixième de leur capacité, puis se redéplacent,
+    si ils sont plein ils se déplacent à chaque tours.
+    Si par chance ils retombent sur le vaisseau mère, ils peuvent se vider et reprendre leur errance. 
+*/
+
+/* 
+Inventaire des fonctions pratiques pour donner des directives au bot :
+    
+    spawn() : fait apparaitre un petit vaisseau sur la case du vaisseau mère
+    stay_still() : petit vaisseau reste sur place pour cette frame
+    move() : déplace le vaisseau dans une direction donnée (cardinale)
+
+    Getter :
+        is_full() : petit vaisseau plein
+        is_occupied() : est ce que la case est occupée
+        at() : récupère position de l'élément (vaisseau mère ou petit vaisseau, case...)
+*/
+
+/*
+Inventaire des fonctions de gestion de partie :
+    push_back() : ajouter une instruction à la liste des instructions à faire
+    end_turn() : regarde si la liste d'instruction est terminée
+    update_frame() : actualise l'entièreté de la partie est initialise le tour actuel
+*/
